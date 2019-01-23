593c7c683cb2efccd9032f5af078a2c16c0c3efd
Pankaj <fusion.pankaj@gmail.com>
2019-01-23 17:28:09 -0500

    now successfully analyzes and exports to png

--- extract.py
+++ extract.py
@@ -4,8 +4,10 @@ import pandas
 import math
 import numpy as np
 from collections import Counter #for counting number of values in a list
-import re
+from datetime import datetime #converting the datetime
 
+time_format = '%b %d, %Y, %I:%M %p'
+oct1 = datetime.strptime('Oct 1, 2018, 12:00 AM', time_format)
 
 
 soup = bs(open("message (440).html"), "html.parser")
@@ -17,94 +19,179 @@ names_list = [names[0][0], names[1][0]]
 
 
 #first I need to replace values with actual
-emo_replaces_dict = {
-'Riju':'Rija',
-'\u2019':"'",
-'\U0001f600':'~grinning face~',
-'\U0001f603':'~grinning face with big eyes~',
-'\U0001f604':'~grinning face with smiling eyes~',
-'\U0001f601':'~beaming face with smiling eyes~',
-'\U0001f606':'~grinning squinting face~',
-'\U0001f605':'~grinning face with sweat~',
-'\U0001f923':'~rolling on the floor laughing~',
-'\U0001f602':'~face with tears of joy~',
-'\U0001f642':'~slightly smiling face~',
-'\U0001f643':'~upside-down face~',
-'\U0001f609':'~winking face~',
-'\U0001f60a':'~smiling face with smiling eyes~',
-'\U0001f607':'~smiling face with halo~',
-'\U0001f60d':'~smiling face with heart-eyes~',
-'\U0001f929':'~star-struck~'
+# numb_to_emo_dict = {
+# '~1~':'~grinning face~',
+# '~2~':'~grinning face with big eyes~',
+# '~3~':'~grinning face with smiling eyes~',
+# '~4~':'~beaming face with smiling eyes~',
+# '~5~':'~grinning squinting face~',
+# '~6~':'~grinning face with sweat~',
+# '~7~':'~rolling on the floor laughing~',
+# '~8~':'~face with tears of joy~',
+# '~9~':'~slightly smiling face~',
+# '~10~':'~upside-down face~',
+# '~11~':'~winking face~',
+# '~12~':'~smiling face with smiling eyes~',
+# '~13~':'~smiling face with halo~',
+# '~14~':'~smiling face with heart-eyes~',
+# '~15~':'~star-struck~'
+# }
+
+emo_replaces_numb_dict = {
+u'\u2019':"'",
+u'\U0001f600':'~1~',
+u'\U0001f603':'~2~',
+u'\U0001f604':'~3~',
+u'\U0001f601':'~4~',
+u'\U0001f606':'~5~',
+u'\U0001f605':'~6~',
+u'\U0001f923':'~7~',
+u'\U0001f602':'~8~',
+u'\U0001f642':'~9~',
+u'\U0001f643':'~10~',
+u'\U0001f609':'~11~',
+u'\U0001f60a':'~12~',
+u'\U0001f607':'~13~',
+u'\U0001f60d':'~14~',
+u'\U0001f929':'~15~',
+u'\U0001f44d':'~16~',
+u'\U0001f937':'~17~',
+u'\U0001f3fd':'~18~',
+u'\u200d\u2640\ufe0f':'~19~',
+u'\U0001f914':'~20~',
 }
 
 
-
-#remove all emos and '
-tds1 = [x.encode('ascii', 'ignore').decode('ascii') for x in tds]
-
-
-
-
+#game_to_remove
+game_contents_res = [ "[A-z]* played back, now it is your turn!", "[A-z] has started a game with you. It's your turn", "[A-z]* has invited you to play [A-z]* with them", "[A-z]* sent an attachment."]
+geme_contents_re = [re.compile(x) for x in game_contents_res]
 
 #experiment
 tds1 = tds
-tds1 = tds1[5:50]
 
 
-for key,value in emo_replaces_dict.iteritems():
-    print "Replacing " + key + " with " + value
-    tds2 = [x.replace('\u2019',"'",) for x in tds1]
-    
-tds2[10]
+r_date = re.compile("[A-z]+\s+\d+,\s+\d+,\s+\d:\d+\s+[A-z]+")
+
+r_liked = re.compile("~[0-9][0-9]~"+names_list[0]+"|~[0-9][0-9]~"+names_list[1])
+
+df = pandas.DataFrame({"instant":[], "name":[], "content":[], "time":[]})
+tds2 = list(reversed(tds1[7:-1]))
+
+for key,value in emo_replaces_numb_dict.iteritems():
+    tds2 = [x.replace(key,value) for x in tds2]
+
+for value in tds2:
+    if value in names_list:
+        name = value
+        df = df.append({'instant':date, 'name':name, "content":content}, ignore_index=True) 
+    elif re.match(r_date, value) is not None:
+        date = value   
+    elif re.match(r_liked, value) is not None:
+        pass
+    elif re.match(geme_contents_re[0], value) is not None: #removing game invites and blah blah
+        content = ""
+    elif re.match(geme_contents_re[1], value) is not None:
+        content = ""
+    elif re.match(geme_contents_re[2], value) is not None:
+        content = ""
+    elif re.match(geme_contents_re[3], value) is not None:
+        content = ""
+    else:
+        content = value
+        
+        
+#remove remaining all emos and '
+#time conversion
+df['time'] = ""
+def string_to_secs(x):
+    return (datetime.strptime(x, time_format)-oct1).total_seconds()
+
+def ignore_unknown_emos(x):
+    return x.encode('ascii', 'ignore').decode('ascii')
+
+df['time'] = df['instant'].apply(string_to_secs)
+df['content'] = df['content'].apply(ignore_unknown_emos)
+
+df.to_csv("output.csv") #to a csv file
 
 
+#plots are here
+#removing the emos
+
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import pillow
+import matplotlib.pyplot as plt
+
+text = ""
+#text = " ".join(x for x in df.content[df.name == "Pankaj Mani Dahal"])
+text = " ".join(x for x in df.content)
+text = re.sub("~[0-9]*~", "",text)
+text = re.sub("~[0-9]*~", "",text)
+text = re.sub(r"\\/"," ",text)
+text = re.sub(r"[^a-zA-Z ']","",text)
+text = re.sub(r"WB"," ",text)
+text = re.sub(r"LU"," ",text)
+text = re.sub(r"\s+"," ",text)
 
 
-# tds_1 = [tds[i] for i in range(0, len(tds), 3)]
-# tds_2 = [tds[i] for i in range(1, len(tds), 3)]
-# tds_3 = [tds[i] for i in range(2, len(tds), 3)]
+# Create stopword list:
+stopwords = set(STOPWORDS)
+stopwords.update(["Riju", "sent", "photo", "ko", "ni", "maa","video", "haru", "bhane", "ra", "Kei"])
 
-pandas.DataFrame(tds_sublists [1:], columns=tds_sublists [0])
-    
+# Generate a word cloud image
+wordcloud = WordCloud(stopwords = stopwords, width = 2048, height = 1536, max_font_size=500, max_words=200, background_color="white").generate(text)
 
-#using regular expression
-r = re.compile("")
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
 
-
-
-size = math.ceil(len(tds)/3)
+plt.savefig("img/wordcloud.png", format="png")
 
 
-df = pandas.DataFrame(np.array(tds).reshape(3,100), columns = list("abc"))
+#other plots
+#frequency
+def get_frequency(x):
+    return len(re.findall("\w+", x))
 
-extracteddf = pandas.DataFrame({'1':[], '2':[], '3':[]})
+df['frequency'] = df['content'].apply(get_frequency)
+df['freq1'] = df.frequency[df.name == names_list[0]].fillna(0)
+df['freq2'] = df.frequency[df.name == names_list[1]].fillna(0)
 
+df['cumfreq1'] = df['freq1'].cumsum()
+df['cumfreq2'] = df['freq2'].cumsum()
 
-
-
-
-
-
-df = pandas.DataFrame({"col1": list(tds2)})
-df.to_csv("./file.csv", sep=',',index=False)
-
-
-
-
-for values in tds:
-    
-    
-    
+plt.plot(df.time[df['name']==names_list[0]].values, df.cumfreq1[df['name']==names_list[0]].values, c = 'blue', label = names_list[0], linewidth = 2)
+plt.plot(df.time[df['name']==names_list[1]].values, df.cumfreq2[df['name']==names_list[1]].values, c = 'red', label = names_list[1], linewidth = 2)
+plt.show()
 
 
+#get game scores
+def get_scores(x):
+    a = re.findall("(LU\d+-\d+)|(WB\d+-\d+)", x)
+    if a==[]:
+        return ""
+    elif a[0][0] == "":
+        return a[0][1]
+    else:
+        return a[0][0]
 
+def get_diff(x):
+    a = re.findall("(\d+)-(\d+)", x)
+    print a
+    return (int(a[0][1]) - int(a[0][0]))    
 
+#get scores in scores column
+df['scores'] = df['content'].apply(get_scores)
+df_games = df[df.scores !=""]
 
+df_games['diff'] = df_games['scores'].apply(get_diff)
 
-month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+WB = plt.scatter(df_games[df_games.scores.str.contains("WB")]['time'].values, df_games[df_games.scores.str.contains("WB")]['diff'].values, c = 'red', label = names_list[0], marker='x')
+LU = plt.scatter(df_games[df_games.scores.str.contains("LU")]['time'].values, df_games[df_games.scores.str.contains("LU")]['diff'].values, c = 'blue', label = names_list[0],  marker='o')
 
-tds = [x for x in tds if x not in remove_list]
-for month in month_list:
-    tds = [x for x in tds if month not in x]
+plt.tick_params(labelsize=8)
+
+plt.legend((WB, LU), ("Word Blitz", "Ludo"), loc='lower left', fontsize=8)
+plt.savefig("img/WB_LU.png", format="png")
+
 
 
