4fc30f095c45abc17fe974fa02e2a475a2fad871
Pankaj <fusion.pankaj@gmail.com>
2019-02-27 10:25:50 -0500

    added assets, autodetect games and add, arguments and default values

--- extract.py
+++ extract.py
@@ -3,17 +3,31 @@ import pandas
 import math
 import numpy as np
 import sys
-from bs4 import BeautifulSoup as bs
+from PIL import Image
 from collections import Counter  # for counting number of values in a list
 from datetime import datetime  # converting the datetime
-from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup as bs
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-filename = "message(440).html"
-filename = sys.argv[1]
 
-if sys.argv[1] in ["-h", "-h"]:
-    print "Usage: python extract.py <filename.html>"
+try:
+    filename = sys.argv[1]
+    if sys.argv[1] in ["-h", "-h"]:
+        print "Usage: python extract.py <filename.html>"
+except:
+    print ("html filename not provided, using default messages.html")
+    filename = "message.html"
+
+#assets
+path_case_insensitive_list = r"assets/case_insensitive.txt"
+path_stopwords_list = r"assets/stopwords.txt"
+path_replacement_dict_list = r"assets/replace.txt"
+
+#path
+path_mask_png = 'img/heart.png'
+
+
 
 time_format = '%b %d, %Y, %I:%M %p'
 first_chat_date = ""
@@ -27,25 +41,6 @@ tds = soup.find_all(text=True)
 names = Counter(x for x in tds).most_common(2)
 names_list = [names[0][0], names[1][0]]
 name_string = names_list[0].split(" ")[0] + "_" + names_list[1].split(" ")[0] + "_"
-
-# first I need to replace values with actual
-# numb_to_emo_dict = {
-# '~1~':'~grinning face~',
-# '~2~':'~grinning face with big eyes~',
-# '~3~':'~grinning face with smiling eyes~',
-# '~4~':'~beaming face with smiling eyes~',
-# '~5~':'~grinning squinting face~',
-# '~6~':'~grinning face with sweat~',
-# '~7~':'~rolling on the floor laughing~',
-# '~8~':'~face with tears of joy~',
-# '~9~':'~slightly smiling face~',
-# '~10~':'~upside-down face~',
-# '~11~':'~winking face~',
-# '~12~':'~smiling face with smiling eyes~',
-# '~13~':'~smiling face with halo~',
-# '~14~':'~smiling face with heart-eyes~',
-# '~15~':'~star-struck~'
-# }
 
 # converting all the unicode emos to numbers
 emo_replaces_numb_dict = {
@@ -101,27 +96,32 @@ def ignore_unknown_emos(x):
 
 # get game scores
 def get_scores(x):
-    a = re.findall("(LU\d+-\d+)|(WB\d+-\d+)", x)
+    re_exp = ""
+    for games in game_names:
+        re_exp = re_exp + "^(?! )(" + games + "\d+-\d+)|"
+    re_exp = re_exp[:-1]
+    # print re_exp
+    a = re.findall(re_exp, x)
     if a == []:
         return ""
-    elif a[0][0] == "":
-        return a[0][1]
-    else:
-        return a[0][0]
+    a = list(a[0])
+    a = [x for x in a if x != ""]
+    return a[0]
 
 
-# get difference between scores
+# get difference between scores, b-a
 def get_diff(x):
     a = re.findall("(\d+)-(\d+)", x)
     return (int(a[0][1]) - int(a[0][0]))
 
 
-# get time duration in minutes
+# get time duration in minutes from call log
 def get_time(x):
     if re.match("Duration:\s+(\d*)\s+minutes", x):
         return int(re.findall("Duration:\s+(\d*)\s+minutes", x)[0])
     else:
-        return float(re.findall("Duration:\s+(\d*)\s+seconds", x)[0]) / 60
+        return 0  # seconds is not working for some reason so removed the feature
+        # return float(re.findall("Duration:\s+(\d*)\s+seconds", x)[0]) / 60
 
 
 # datetime converted to x-axis label, global date_time_df updated
@@ -143,7 +143,7 @@ def get_time_labels():
     every_n_value = int(math.floor(len(date_time_df) / 6))
     if every_n_value > 1:
         date_time_df = date_time_df.iloc[::every_n_value, :]
-    print date_time_df
+    # print date_time_df
     date_time_df = date_time_df.astype(int).astype(str)
     date_time_df['label'] = date_time_df['Month'] + '/' + date_time_df['Year']
 
@@ -193,26 +193,67 @@ my_df['time'] = my_df['instant'].apply(update_dates_and_convert_to_seconds)
 my_df['content'] = my_df['content'].apply(ignore_unknown_emos)
 
 # creating wordcloud
-# cleaning up
+
+# before get the name of games
+
 text = " ".join(x for x in my_df.content)
-text = re.sub("~[0-9]*~", "", text)  # substitute ~##~ with ""
-text = re.sub(r"\\/", " ", text)  # substitute / with ""
-text = re.sub(r"[^a-zA-Z ']", "", text)  # anything but A-z and space are removed
-text = re.sub(r"WB", " ", text)  # WB and LU are removed
-text = re.sub(r"LU", " ", text)
-text = re.sub(r"\s+", " ", text)  # multiple spaces converted to single space
+game_names = list(set((re.findall("([A-Z]+)\d+-\d+", text))))
+
+# cleaning up
+# removing game names
+for name in game_names:
+    text = re.sub(name, " ", text)
+
+re_sub_dict = {"~[0-9]*~": "", r"\\/": " ", r"[^a-zA-Z ']": "", r"\s+": " ", r"The video chat ended.": " ",
+               r"sent a photo.": " ", r"sent a video": " ", r"sent an attachment.": " ",
+               r"missed a video chat with": " ", r'[A-z ]* sent a GIF[A-z. ]*': " "}
+
+# substitute ~##~ with ""
+# substitute / with ""
+# anything but A-z and space are removed
+# multiple spaces converted to single space
+
+for key, value in re_sub_dict.items():
+    text = re.sub(key, value, text)
+
+# remove case insensitivity
+with open(path_case_insensitive_list, 'r') as f:
+    case_insensitive_list = f.read().splitlines()
+
+for value in case_insensitive_list:
+    text = re.sub(r"(?i)(" + value + ")", value, text)  # missed a video chat with
 
 # Create stopword list:
 stopwords = set(STOPWORDS)
-stopwords.update(
-    [names_list[0].split(" ")[0], names_list[1].split(" ")[0], "sent", "photo", "ko", "ni", "maa", "video", "haru",
-     "bhane", "ra", "Kei", "le", "nai", "ta", "cha", "chai", "na", "ki", "thiye"])
+
+with open(path_stopwords_list, 'r') as f:
+    nepali_stopwords = f.read().splitlines()
+
+names = [names_list[0].split(" ")[0], names_list[1].split(" ")[0]]
+stopwords.update(nepali_stopwords)
+stopwords.update(names)
+
+# pre-processing the text a little bit
+with open(path_replacement_dict_list, 'r') as f:
+    replacement_dict = f.read().splitlines()
+replacement_dict = {x.split(" ")[0]: x.split(" ")[1] for x in replacement_dict}
+
+# replace
+for key, value in replacement_dict.iteritems():
+    text = text.replace(key, value)
+
+# add image
+mask = np.array(Image.open(path_mask_png))
+image_colors = ImageColorGenerator(mask)
 
 # Generate a word cloud image
-wordcloud = WordCloud(stopwords=stopwords, width=2880, height=1620, max_font_size=800, max_words=400,
-                      background_color="white").generate(text)
+wordcloud = WordCloud(stopwords=stopwords, max_font_size=800, max_words=400, mode="RGBA", mask=mask,
+                      font_path="comic.ttf", background_color="white").generate(text)
+# wordcloud = WordCloud(stopwords=stopwords, width=2970, height=2100, max_font_size=800, max_words=400, background_color="white", font_path="comic.ttf", mode="R").generate(text)
+
 plt.close()
-plt.imshow(wordcloud, interpolation='bilinear')
+plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation='quadric')
+# plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.savefig("img/" + name_string + "wordcloud.png", format="png", dpi=1500)
 
@@ -243,16 +284,21 @@ my_df['scores'] = my_df['content'].apply(get_scores)
 df_games = my_df[my_df.scores != ""]
 df_games['diff'] = df_games['scores'].apply(get_diff)
 plt.close()
-WB = plt.scatter(df_games[df_games.scores.str.contains("WB")]['time'].values,
-                 df_games[df_games.scores.str.contains("WB")]['diff'].values, c='red', marker='x')
-LU = plt.scatter(df_games[df_games.scores.str.contains("LU")]['time'].values,
-                 df_games[df_games.scores.str.contains("LU")]['diff'].values, c='blue', marker='o')
+
+a = {}
+c = ['red', 'green', 'blue', 'yellow', 'pink']
+m = ['x', 'o', '+', '*', 't']
+count = 0
+for name in game_names:
+    a[name] = plt.scatter(df_games[df_games.scores.str.contains(name)]['time'].values,
+                          df_games[df_games.scores.str.contains(name)]['diff'].values, c=c[count], marker=m[count])
+    count = count + 1
 plt.tick_params(labelsize=8)
 plt.xticks(list(date_time_df.Seconds.astype(int).values)[2:],
            list(date_time_df.label.values)[2:])  # first 2 months no play
-plt.legend((WB, LU), ("Word Blitz", "Ludo"), loc='lower left', fontsize=8)
-plt.savefig("img/" + name_string + "WB_LU.png", format="png")
 
+plt.legend(a.values(), a.keys(), loc='lower left', fontsize=8)
+plt.savefig("img/" + name_string + "games_plot.png", format="png")
 
 # plot calls
 for value in tds2:
@@ -304,8 +350,7 @@ plt.savefig("img/" + name_string + "callinminutes.png", format="png")
 my_df.to_csv("output/" + name_string + "report.csv")  # to a csv file
 
 
-
-#combined plot
+# combined plot
 plt.close()
 
 fig, ax1 = plt.subplots()
@@ -317,12 +362,11 @@ s11 = my_df.cumfreq1[my_df['name'] == names_list[0]].values
 s12 = my_df.cumfreq2[my_df['name'] == names_list[1]].values
 s3 = time_df.cumtime.values
 
-
 ax1.plot(t11, s11, c='blue', linewidth=2, label=names_list[0].split(" ")[0])
 ax1.plot(t12, s12, c='red', linewidth=2, label=names_list[1].split(" ")[0])
 ax1.set_xlabel('Day')
 # Make the y-axis label, ticks and tick labels match the line color.
-ax1.set_ylabel('Words',color='blue')
+ax1.set_ylabel('Words', color='blue')
 ax1.tick_params('y', colors='blue')
 
 ax2 = ax1.twinx()
@@ -332,7 +376,7 @@ ax2.set_ylabel('time(minutes)', color='green')
 ax2.tick_params('y', colors='green')
 
 fig.tight_layout()
-#fig.xticks(list(date_time_df.Seconds.astype(int).values), list(date_time_df.label.values))
+# fig.xticks(list(date_time_df.Seconds.astype(int).values), list(date_time_df.label.values))
 
-plt.show()
-#plt.savefig("img/" + name_string + "combined.png", format="png")
+
+plt.savefig("img/" + name_string + "combined.png", format="png")
